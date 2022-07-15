e2b36a19475e3dd041f287917105e52d405fcc48
Mateusz Pszczoła <matip03211@gmail.com>
2022-07-15 15:27:35 +0200

    map render

--- game/arena.go
+++ game/arena.go
@@ -2,24 +2,41 @@ package game
 
 import "github.com/gdamore/tcell"
 
-type Arena struct {
-	Width    int
-	Height   int
-	FruitPos []int
+type arena struct {
+	width    int
+	height   int
+	fruitPos []int
 }
 
-func InitArena(s tcell.Screen, fruitPos []int) Arena {
+func InitArena(s tcell.Screen, fruitPos []int) arena {
 	w, h := s.Size()
-	return Arena{
-		w / 2,
-		h / 2,
+	return arena{
+		w - 30,
+		h - 5,
 		fruitPos,
 	}
 }
 
-func (arena *Arena) RenderArena(s tcell.Screen) {
-
-	style := tcell.Style.Background(tcell.StyleDefault, tcell.ColorDefault).Foreground(tcell.ColorRebeccaPurple)
+func (arena *arena) RenderArena(s tcell.Screen) {
+	style := tcell.Style.Background(tcell.StyleDefault, tcell.ColorDefault).Foreground(tcell.ColorDefault)
 	s.SetStyle(style)
-	s.SetContent(1, 1, 'H', nil, style)
+	for i := 30; i < arena.width; i++ {
+		s.SetContent(i, arena.height, '─', nil, style)
+		s.SetContent(i, arena.height-20, '─', nil, style)
+	}
+
+	for i := 6; i < arena.height; i++ {
+		s.SetContent(arena.width, i, '│', nil, style)
+		s.SetContent(arena.width-61, i, '│', nil, style)
+	}
+
+	s.SetContent(arena.width-61, arena.height-20, '┌', nil, style)
+	s.SetContent(arena.width, arena.height-20, '┐', nil, style)
+	s.SetContent(arena.width, arena.height, '┘', nil, style)
+	s.SetContent(arena.width-61, arena.height, '└', nil, style)
+
+	// '┌'
+	// '└'
+	// '┐'
+	// '┘'
 }
